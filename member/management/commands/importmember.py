f537cb5706e0ee7dcea3154d784de5f127993fe4
sshwsfc <sshwsfc@gmail.com>
2014-10-13 12:41:04 +0800

    修正导入程序

--- member/management/commands/importmember.py
+++ member/management/commands/importmember.py
@@ -9,7 +9,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         fname = os.path.join(settings.BASE_DIR, 'etc/4class.csv')
-        lines = file(fname).readlines()[1:2]
+        lines = file(fname).readlines()
         now = timezone.now()
         for line in lines:
             l = line.split(',')
@@ -17,7 +17,7 @@ class Command(BaseCommand):
                 Member.objects.get(number=l[0])
             except Exception:
                 member = Member(username=l[1], email=l[3], classnum=4, number=l[0], weixin='xx', mobile=l[2], \
-                  birthday=datetime.date.today(), gender='m',is_active=True,
+                  birthday=datetime.date.today(), gender='m',is_active=True, qq=l[4],
                           is_superuser=False, last_login=now)
                 member.set_password('123')
                 member.save()
